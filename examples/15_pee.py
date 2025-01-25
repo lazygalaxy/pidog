9c9278776253a33e966f205bbca434fef77422c5
LazyGalaxy <vangos@lazygalaxy.com>
2025-01-25 13:40:16 +0100

    improve template

--- examples/15_pee.py
+++ examples/15_pee.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python3
 import time
 from pidog import Pidog
-from time import sleep
-from preset_actions import howling
+from preset_actions import bark
 
+t = time.time()
 my_dog = Pidog()
-
-sleep(0.5)
+my_dog.do_action("stand", speed=80)
+my_dog.wait_all_done()
+time.sleep(0.5)
 
 stand = my_dog.legs_angle_calculation([[0, 80], [0, 80], [30, 75], [30, 75]])
-pee = my_dog.legs_angle_calculation([[0, 80], [0, 80], [30, 75], [75, 75]])
+pipi = my_dog.legs_angle_calculation([[0, 80], [0, 80], [30, 75], [75, 75]])
 
 
 def pee():
-    my_dog.legs_move([stand], speed=70)
-    my_dog.wait_all_done()
-    my_dog.legs_move([pee], speed=50)
-    my_dog.wait_all_done()
-    time.sleep(3.0)
+    while True:
+        my_dog.legs_move([stand], speed=70)
+        my_dog.wait_all_done()
+        my_dog.legs_move([pipi], speed=70)
+        my_dog.wait_all_done()
+        time.sleep(3.0)
 
 
 if __name__ == "__main__":
     try:
         while True:
             pee()
-            time.sleep(1.0)
+            time.sleep(0.01)
     except KeyboardInterrupt:
         pass
     except Exception as e:
