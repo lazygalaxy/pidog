14cdc7ed9158aa7b65685351818a889cffe638c1
LazyGalaxy <vangos@lazygalaxy.com>
2025-01-26 08:21:52 +0100

    remove excessive waiting

--- examples/15_peeing.py
+++ examples/15_peeing.py
@@ -9,19 +9,14 @@ stand = my_dog.legs_angle_calculation([[0, 80], [0, 80], [30, 75], [30, 75]])
 left_pipi = my_dog.legs_angle_calculation([[0, 40], [0, 80], [30, 75], [30, 0]])
 right_pipi = my_dog.legs_angle_calculation([[0, 80], [0, 40], [30, 0], [30, 75]])
 
-my_dog.do_action("stand", speed=80)
-my_dog.wait_all_done()
-
-
 def pee():
     pee_side = False
     while True:
-        # standing position
-        my_dog.legs_move([stand], speed=70)
-        my_dog.wait_all_done()
+        # standing position with head staright
+        body_reset(my_dog)
+        time.sleep(1.0)
 
         # looking around if anyone is looking!
-        time.sleep(1.0)
         head_turn(my_dog, 40)
         my_dog.wait_all_done()
         time.sleep(1.0)
@@ -43,10 +38,14 @@ def pee():
         my_dog.do_action("wag_tail", step_count=5, speed=99)
         time.sleep(5.0)
 
+def body_reset(my_dog):
+    head_turn(my_dog)
+    my_dog.legs_move([stand], speed=70)
+    my_dog.wait_all_done()
 
 def head_turn(my_dog: Pidog, x_posi: int = 0, y_posi: int = 0):
     yrp = [0, 0, y_posi]
-    head_posi = [x_posi + yrp[0], 0 + yrp[1], 0 + yrp[2]]
+    head_posi = [[x_posi + yrp[0], 0 + yrp[1], 0 + yrp[2]]]
     my_dog.head_move(head_posi, speed=92)
     my_dog.wait_all_done()
 
